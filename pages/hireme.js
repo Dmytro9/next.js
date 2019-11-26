ee45eaf0e5673dd3dd768f00274ee63c80a652fc
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2019-11-26 11:29:02 +0200

    routes, styles, head of page with title link etc Head component, nprogress

---
+++ pages/hireme.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout title='Hire me'>
+    <p>You can hire me at <a href='mailto:dima@gmail.com'>dima@gmail.com</a></p>
+  </Layout>
+);
