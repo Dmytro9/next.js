73847041ee1f32c8108320b3d30919583743c42f
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2019-11-29 13:36:38 +0200

    fetch data

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,21 @@
 import { Component } from "react";
 import Layout from "../components/Layout";
-
-// export default () => (
-//   <Layout title='About'>
-//     <p>A JS programmer</p>
-//     <img src="/static/javascript-logo.png" alt="logo" height="200px" />
-//   </Layout>
-// );
+import fetch from "isomorphic-unfetch";
 
 export default class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch("http://api.github.com/users/Dmytro9");
+    const data = await res.json();
+    return { user: data };
+  }
+
   render() {
+    const data = this.props.user;
+
     return (
       <Layout title="About">
-        <p>A JS programmer</p>
-        <img src="/static/javascript-logo.png" alt="logo" height="200px" />
+        <p>{data.name}</p>
+        <img src={data.avatar_url} alt="logo" height="200px" />
       </Layout>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import {useSelector} from "react-redux";
+import Link from "next/link";
 
-const Index = ({custom}) => {
-    const foo = useSelector((state) => state.foo);
 
-      return (
-        <Layout title='Home'>
-            <div>Prop from Redux {foo}</div>
-            <div>Prop from getInitialProps {custom}</div>
-        </Layout>
-      )
+const Index = () => {
+    return (
+    <Layout title='Home'>
+        <Link href="/about">
+            <a>Go to about</a>
+        </Link> 
+        <p>Welcome to the home page</p>
+    </Layout>
+    )
 }
 
-Index.getInitialProps = ({store, isServer, pathname, query}) => {
-    store.dispatch({type: 'FOO', payload: 'sdcsdc'});
-    return {custom: 'custom'}; 
-  };
+// Index.getInitialProps = ({store, isServer, pathname, query}) => {
+//     store.dispatch({type: 'FOO', payload: 'sdcsdc'});
+//     return {custom: 'custom'}; 
+//   };
 
   export default Index;
