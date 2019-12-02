920200d3f3468aba1e2513eeeb8dde262f8a4e50
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2019-12-02 10:26:06 +0200

    Error page

---
+++ pages/_error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default ({statusCode}) => {
+  return (
+    <Layout title="Error">
+        {statusCode ? `Could not load your user data. Status Code ${statusCode}` : <p>Couldn't get that page, sorry! </p> }
+    </Layout>
+  );
+};
--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,27 @@
 import { Component } from "react";
 import Layout from "../components/Layout";
+import Error from './_error';
 import fetch from "isomorphic-unfetch";
 
 export default class About extends Component {
   static async getInitialProps() {
     const res = await fetch("http://api.github.com/users/Dmytro9");
+    const statusCode = res.status > 200 ? res.status : false;
     const data = await res.json();
-    return { user: data };
+    return { user: data, statusCode };
   }
 
   render() {
-    const data = this.props.user;
+    const {user, statusCode} = this.props;
+
+    if (statusCode) {
+      return <Error statusCode={statusCode} />
+    }
 
     return (
       <Layout title="About">
-        <p>{data.name}</p>
-        <img src={data.avatar_url} alt="logo" height="200px" />
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="logo" height="200px" />
       </Layout>
     );
   }
